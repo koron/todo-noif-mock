2a40b957303e77d75f1801a27d2162bd12231e47
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-01-20 23:07:06 +0900

    generate mock by koron/mockgo

--- service/repository.go
+++ service/repository.go
@@ -1,5 +1,7 @@
 // +build !mock
 
+//go:generate mockgo -package ../repository Repository
+
 package service
 
 import "github.com/koron/todo-noif-mock/repository"
--- service/todo_service_test.go
+++ service/todo_service_test.go
@@ -1,19 +1,32 @@
 package service
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/koron/todo-noif-mock/do"
+)
 
 func TestServiceCreate(t *testing.T) {
 	r := &Repository{
 		// モック用のRepositoryの初期化をここに書く
+		Insert_Rs: []*RepositoryInsert_R{
+			{&do.Todo{ID: 123, Task: "mytask1"}, nil},
+		},
 	}
 	srv := &Service{repos: r}
-	d, err := srv.PostTodo("テストのタスク1")
+	d, err := srv.PostTodo("in_task1")
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// TODO: dをチェックする
-	_ = d
-
-	// TODO: Createが正しく呼ばれたか`r`の中身をチェックするなど
+	// dをチェックする
+	if !reflect.DeepEqual(d, &do.Todo{ID: 123, Task: "mytask1"}) {
+		t.Fatal("unexpected response")
+	}
+	// Insertが正しく呼ばれたかチェック
+	if !reflect.DeepEqual(r.Insert_Ps, []*RepositoryInsert_P{
+		{&do.Todo{Task: "in_task1"}},
+	}) {
+		t.Fatal("unexpected Insert calls")
+	}
 }
